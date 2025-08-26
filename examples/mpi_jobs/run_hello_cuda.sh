bc4255d3cefd259ec379d5d0a0470c00b6ffb7b9
rohanbabbar04 <rohanbabbar0408@gmail.com>
2025-08-26 16:02:01 +0530

    Add example of hostfile

--- examples/mpi_jobs/run_hello_cuda.sh
+++ examples/mpi_jobs/run_hello_cuda.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-MASTER_HOSTNAME=$1
-WORKER_HOSTNAMES=$2
-ALL_HOSTNAMES="$MASTER_HOSTNAME,$WORKER_HOSTNAMES"
-
 module load mpich
 mkdir -p /mnt/share1/cc
 nvcc -ccbin mpicxx -o /mnt/share1/cc/hello /home/cc/hello.cu
-mpirun -ppn 1 --host "$ALL_HOSTNAMES" /mnt/share1/cc/hello
+mpirun -ppn 1 --hostfile /etc/mpi/hostfile /mnt/share1/cc/hello
